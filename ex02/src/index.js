bd02636a488a37069d5c39159f0a1cbd5e47b896
unknown <sanninm>
2021-09-23 23:42:19 +0200

    Multi-Dimensional Arrays

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -4,19 +4,21 @@ temps = [
     tuesday = [29, 27, 55, 36],
     wednesday = [17, 27, 42, 46],
     thursday = [29, 52, 21, 64],
-    friday = [91, 27, 31, 61],
+    friday = [91, 27, 31, 61]
 ]
 // End of creating an array 
 function myArrayFunction(arr) {
     var newTemps = [...arr];
     var averageDayTemp = [];
    // Only change code below this line 
-   for (var i=0; i<newTemps.lenght; i++) {
-       for (var j=0; j<4; j++) {
-           avg = arr[1][j] + arr[2][j++];
-           
-       }
-       averageDayTemp.push(avg);
+   for (i=0; i<newTemps.length; i++) {
+       var average = 0;
+       var sum = 0;
+       for (j=0; j<newTemps[i].length; j++) {
+           sum+=temps[i][j];
+        }
+        average=sum/4
+       averageDayTemp.push(average);
    }
    
    // Only change code above this line
