5149e64247af5d7bdc5721d473c2108f1ca231e5
unknown <sanninm>
2021-09-23 23:52:06 +0200

    Nest one Array within Another Array

---
+++ ex05/src/index.js
@@ -0,0 +1,8 @@
+   // Only change code below this line 
+   myNestedArry = [["Toblerone", 6], ["Milka", 3]];
+   function myNestedFunction(arr) {
+    return arr;
+   // Only change code above this line
+   }
+   console.log(myNestedFunction(myNestedArry));
+   module.exports = myNestedFunction;
